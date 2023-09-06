fb2a92b271abf61957c1f037cbce1afc8b87a38e
Oblivion <56230151+heatdeathnow@users.noreply.github.com>
2023-09-06 09:19:25 -0300

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,9 +1,9 @@
 from openpyxl.worksheet.worksheet import Worksheet
 from dateutil.relativedelta import relativedelta
 from var import lookup_table, banned_words
+from typing import Sequence, Literal
 from unidecode import unidecode
 from pandas import DataFrame
-from typing import Sequence
 from datetime import date
 
 
@@ -11,8 +11,8 @@ def tostr(x: str) -> str:
     """
     Recebe uma string e retorna uma string em caixa alta, sem acentos e sem espaços antes e depois.
     """
-    return unidecode(str(x).upper().strip())
 
+    return unidecode(str(x).upper().strip())
 
 def fill_column(ws: Worksheet, ranges: Sequence, sex: str, title: str | None) -> None:
     """
@@ -20,25 +20,31 @@ def fill_column(ws: Worksheet, ranges: Sequence, sex: str, title: str | None) ->
     argumentos. Função void, ela não retorna nada, apenas modifica a planilha passada.
     """
 
-    if tostr(sex) == 'F' and tostr(title) == 'T':
-        col = 'B'
-    elif tostr(sex) == 'M' and tostr(title) == 'T':
-        col = 'C'
-    elif tostr(sex) == 'F' and tostr(title) == 'D':
-        col = 'D'
-    elif tostr(sex) == 'M' and tostr(title) == 'D':
-        col = 'E'
-    elif tostr(sex) == 'F' and title is None:
-        col = 'F'
-    elif tostr(sex) == 'M' and title is None:
-        col = 'G'
-    else:
-        raise ValueError('Os únicos que essa função aceita para sexo são "M" e "F". E os únicos valores que essa função'
-                         'aceita para titularidade as strings "T" e "D" e o valor None.')
+    match (tostr(sex), tostr(title)):
+        case ('F', 'T'):
+            col = 'B'
+
+        case ('M', 'T'):
+            col = 'C'
+        
+        case ('F', 'D'):
+            col = 'D'
+        
+        case ('M', 'D'):
+            col = 'E'
+        
+        case ('F', None):
+            col = 'F'
+        
+        case ('M', None):
+            col = 'G'
+
+        case _ :
+            raise ValueError(f'Os únicos valores que essa função aceita são "M" e "F" para sexo, e "T" e "D" para titularidade.\n \
+                             Porém fora passado "{sex}" para sexo e "{title}" para titularidade.')
 
     for i in range(3, 13):
         ws[f'{col}{i}'] = ranges[i - 3]
-
 
 def get_sex_from_name(df: DataFrame, index_name: int, row: int, lookup_table: DataFrame) -> str:
     name = tostr(df.loc[row][index_name].split(' ')[0])  # Retira acentos, coloca tudo em maiúsculo e pega o primeiro nome.
@@ -46,17 +52,19 @@ def get_sex_from_name(df: DataFrame, index_name: int, row: int, lookup_table: Da
     try:
         index = lookup_table.nome[lookup_table.nome == name].index[0]  # Acha a posição desse nome na tabela de lookup.
         return lookup_table.sexo[index]  # Retorna o sexo associado ao nome nessa posição.
+    
     except IndexError:  # Caso o nome não esteja na tabela de lookup, use esse algoritmo.
-        if name[-1] in 'AEIYZ':  # Se o nome termina com essas letras, assumir que é feminino.
+        if name[-1] in 'AEIYZS':  # Se o nome termina com essas letras, assumir que é feminino.
             return 'F'
+        
         else:  # Senão, assumir que é masculino.
             return 'M'
-
 
 def get_index(df: DataFrame, names: Sequence) -> int:
     """
     Procura os nomes passados como parâmetro nos títulos das colunas do Dataframe e retorna a sua posição.
     """
+
     if not isinstance(names, Sequence):
         raise TypeError
 
@@ -65,10 +73,10 @@ def get_index(df: DataFrame, names: Sequence) -> int:
         for name in names:
             if tostr(name) in tostr(column) and tostr(column) not in [tostr(word) for word in banned_words]:
                 return i
+            
         i += 1
 
-    raise IndexError
-
+    raise IndexError(f'Não foi possível encontrar {name} nas colunas da planilha passada...')
 
 def get_ranges(df: DataFrame,
                index_name: int | None,
@@ -77,7 +85,7 @@ def get_ranges(df: DataFrame,
                index_title: int | None,
                title: str,
                index_age: int | None,
-               index_birth: int | None) -> list:
+               index_birth: int | None) -> list[int]:
     """
     Retorna uma lista com 10 inteiros, cada um representando a quantidade de pessoas do sexo e titularidade
     especificadas nos parâmetros da função naquela faixa etária específica encontrados no Dataframe. Se não há índice de
@@ -87,51 +95,67 @@ def get_ranges(df: DataFrame,
 
     sex = tostr(sex)
     if sex != 'M' and sex != 'F':
-        raise ValueError('Apenas as strings "M" e "F" são aceitas por essa função.')
+        raise ValueError(f'Apenas as strings "M" e "F" são aceitas por essa função, porém o valor "{sex}" fora passado.')
 
     title = tostr(title)
     if title != 'T' and title != 'D':
-        raise ValueError('Apenas as string "T" e "D" são aceitas por essa função.')
+        raise ValueError(f'Apenas as string "T" e "D" são aceitas por essa função, porém o valor "{title}" fora passado.')
 
     if index_age is None and index_birth is None:
-        raise ValueError('Essa função precisa que pelo menos um desses dois parâmetros tenham valores não-nulos: "index_age" ou "index_birth".')
+        raise ValueError('Essa função precisa que pelo menos um dos dois parâmetros tenham valores não-nulos: "index_age" ou "index_birth".')
+    
     elif index_age is not None:
-        def age(j): return df.loc[j][index_age]
+        def age(index: int) -> int: return df.loc[index][index_age]
+
     else:
-        def age(j): return relativedelta(date.today(), df.loc[j][index_birth].date()).years
+        def age(index: int) -> int: return relativedelta(date.today(), df.loc[index][index_birth].date()).years
 
     if index_sex is None and index_name is None:
-        raise ValueError('Essa função precisa que pelo menos um desses dois parâmetros tenham valores não-nulos: "index_sex" ou "index_name".')
+        raise ValueError('Essa função precisa que pelo menos um dos dois parâmetros tenham valores não-nulos: "index_sex" ou "index_name".')
+    
     elif index_sex is None:
-        def sex_(j): return get_sex_from_name(df, index_name, j, lookup_table)
+        def sex_(index: int) -> str: return get_sex_from_name(df, index_name, index, lookup_table)
+
     else:
-        def sex_(j): return df.loc[j][index_sex]
+        def sex_(index: int) -> str: return df.loc[index][index_sex]
 
     if index_title is None:  # Se não houver titularidade...
-        def stewardness(_): return title  # A última parte dos if statements será sempre verdadeira (não interferirá).
+        def stewardness(_) -> Literal['T', 'D']: return title  # A última parte dos if statements será sempre verdadeira (não interferirá).
+        
     else:
-        def stewardness(j): return 'T' if tostr(df.loc[j][index_title]) in ('TITULAR', 'T') else 'D'
+        def stewardness(index: int) -> Literal['T', 'D']: return 'T' if tostr(df.loc[index][index_title]) in ('TITULAR', 'T') else 'D'
 
     ranges = [0] * 10
     for i in range(len(df.index)):
-        if age(i) <= 18 and sex_(i) == sex and stewardness(i) == title:
-            ranges[0] += 1
-        elif age(i) <= 23 and sex_(i) == sex and stewardness(i) == title:
-            ranges[1] += 1
-        elif age(i) <= 28 and sex_(i) == sex and stewardness(i) == title:
-            ranges[2] += 1
-        elif age(i) <= 33 and sex_(i) == sex and stewardness(i) == title:
-            ranges[3] += 1
-        elif age(i) <= 38 and sex_(i) == sex and stewardness(i) == title:
-            ranges[4] += 1
-        elif age(i) <= 43 and sex_(i) == sex and stewardness(i) == title:
-            ranges[5] += 1
-        elif age(i) <= 48 and sex_(i) == sex and stewardness(i) == title:
-            ranges[6] += 1
-        elif age(i) <= 53 and sex_(i) == sex and stewardness(i) == title:
-            ranges[7] += 1
-        elif age(i) <= 58 and sex_(i) == sex and stewardness(i) == title:
-            ranges[8] += 1
-        elif age(i) >= 59 and sex_(i) == sex and stewardness(i) == title:
-            ranges[9] += 1
+        match (age(i), sex_(i), stewardness(i)):
+            case x, y, z if x <= 18 and y == sex and z == title:
+                ranges[0] += 1
+
+            case x, y, z if x <= 23 and y == sex and z == title:
+                ranges[1] += 1
+
+            case x, y, z if x <= 28 and y == sex and z == title:
+                ranges[2] += 1
+
+            case x, y, z if x <= 33 and y == sex and z == title:
+                ranges[3] += 1
+            
+            case x, y, z if x <= 38 and y == sex and z == title:
+                ranges[4] += 1
+            
+            case x, y, z if x <= 43 and y == sex and z == title:
+                ranges[5] += 1
+            
+            case x, y, z if x <= 48 and y == sex and z == title:
+                ranges[6] += 1
+            
+            case x, y, z if x <= 53 and y == sex and z == title:
+                ranges[7] += 1
+            
+            case x, y, z if x <= 58 and y == sex and z == title:
+                ranges[8] += 1
+            
+            case x, y, z if x >= 59 and y == sex and z == title:
+                ranges[9] += 1
+
     return ranges
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from openpyxl import load_workbook
+from colorama import Fore, Style
+from time import perf_counter
 import pandas as pd
 from calc import *
 from var import *
@@ -6,56 +8,77 @@ import argparse
 
 
 if __name__ == '__main__':
+    runtime = perf_counter()
     parser = argparse.ArgumentParser()
     parser.add_argument('dir')
     args = parser.parse_args()
 
     lookup_sex_dir = 'dados\\sex.csv'  # Diretório para tabela lookup que associa os nomes aos sexos.
     model_dir = 'dados\\model.xlsx'  # Diretório para o arquivo modelo formatado.
-    excel_type = '.xlsx'
 
     if args.dir[-5:] != '.xlsx' and args.dir[-4:] != '.xls':
-        raise ValueError('As únicas extensões aceitas são .xlsx e .xls')
-    elif args.dir[-4:] == '.xls':
-        input_file = args.dir
-        output_file = input_file.replace('.xls', '') + ' - análise.xls'
+        raise ValueError(f'As únicas extensões aceitas são .xlsx e .xls, porém fora passado {args.dir[-5:]}.')
+
     else:
         input_file = args.dir
-        output_file = input_file.replace('.xlsx', '') + ' - análise.xlsx'
+        output_file = input_file.replace('.xlsx', '').replace('.xls', '') + ' - análise.xlsx'
+
     df = pd.read_excel(input_file)
 
     try:
         index_age = get_index(df, lookup_age)
         index_birth = None
+
     except IndexError:
         try:
             index_birth = get_index(df, lookup_birth)
             index_age = None
+
         except IndexError:
-            raise KeyError('Nem idade nem data de nascimento foram encontrados na planilha.')
+            raise KeyError('Incapaz de identificar uma coluna de idade ou de data de nascimento na planilha dada.')
 
     try:
         index_sex = get_index(df, lookup_sex)
         index_name = None
+
     except IndexError:
         try:
             index_name = get_index(df, lookup_names)
             index_sex = None
+
         except IndexError:
-            raise KeyError('Nem sexo nem nome foram encontrados na planilha.')
+            raise KeyError('Incapaz de identificar uma coluna de sexo ou de nome na planilha dada.')
 
     try:
         index_title = get_index(df, lookup_title)
+
     except IndexError:
         index_title = None
+        print(f'{Fore.RED}Incapaz de identificar uma coluna de titularidade na planilha dada. A execução continuará sem a titularidade.{Style.RESET_ALL}')
 
     model = load_workbook(model_dir)
     ws = model.worksheets[0]
+
     if index_title is not None:
+        start_time = perf_counter()
+        print('Calculando a faixa etária das mulheres titulares... ', end = '')
         female_stewards_range = get_ranges(df, index_name, index_sex, 'F', index_title, 'T', index_age, index_birth)
+        print(f'cálculo concluído em {perf_counter() - start_time:.2f} segundos.')
+
+        start_time = perf_counter()
+        print('Calculando a faixa etária dos homens titulares... ', end = '')
         male_stewards_range   = get_ranges(df, index_name, index_sex, 'M', index_title, 'T', index_age, index_birth)
+        print(f'cálculo concluído em {perf_counter() - start_time:.2f} segundos.')
+
+        start_time = perf_counter()
+        print('Calculando a faixa etária das mulheres dependentes... ', end = '')
         female_proteges_range = get_ranges(df, index_name, index_sex, 'F', index_title, 'D', index_age, index_birth)
+        print(f'cálculo concluído em {perf_counter() - start_time:.2f} segundos.')
+
+        start_time = perf_counter()
+        print('Calculando a faixa etária dos homens dependentes... ', end = '')
         male_proteges_range   = get_ranges(df, index_name, index_sex, 'M', index_title, 'D', index_age, index_birth)
+        print(f'cálculo concluído em {perf_counter() - start_time:.2f} segundos.')
 
         fill_column(ws, female_stewards_range, 'F', 'T')
         fill_column(ws, male_stewards_range, 'M', 'T')
@@ -63,10 +86,18 @@ if __name__ == '__main__':
         fill_column(ws, male_proteges_range, 'M', 'D')
 
     else:
+        start_time = perf_counter()
+        print('Calculando a faixa etária das mulheres... ', end = '')
         female_range = get_ranges(df, index_name, index_sex, 'F', index_title, '.', index_age, index_birth)
+        print(f'cálculo concluído em {perf_counter() - start_time:.2f} segundos.')
+
+        start_time = perf_counter()
+        print('Calculando a faixa etária dos homens... ', end = '')
         male_range   = get_ranges(df, index_name, index_sex, 'M', index_title, '.', index_age, index_birth)
+        print(f'cálculo concluído em {perf_counter() - start_time:.2f} segundos.')
 
         fill_column(ws, female_range, 'F', None)
         fill_column(ws, male_range, 'F', None)
 
     model.save(output_file)
+    print(f'A execução foi bem-sucedida e terminada em {perf_counter() - runtime:.2f} segundos.')
